02d7ce54f05941bb1bd28fdc950b573c126a001e
Pelmeshic <132111094+Pelmeshic@users.noreply.github.com>
2023-05-18 20:07:38 +1100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,30 @@
+# Имопртирую библиотеки
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QRadioButton
+
+# Создание окна
+
+app = QApplication([])
+win = QWidget()
+win.show()
+
+# Создание лаяута
+
+main_layout = QHBoxLayout() 
+
+# Создание кнопки
+
+button_1 = QPushButton()
+button_2 = QPushButton()
+button_3 = QPushButton()
+
+# Соединение
+
+main_layout.addWidget(button_1)
+main_layout.addWidget(button_2)
+main_layout.addWidget(button_3)
+
+
+win.setLayout(main_layout) #устанаваливаем окну win layout - main_layout
+app.exec_()
